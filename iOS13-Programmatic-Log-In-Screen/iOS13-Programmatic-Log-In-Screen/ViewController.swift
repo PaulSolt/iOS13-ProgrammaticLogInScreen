79909b9edd12708cc70efcfd4bfa92c82e82f254
Paul Solt <PaulSolt@gmail.com>
2020-03-27 12:13:10 -0400

    Adde starter project

---
+++ iOS13-Programmatic-Log-In-Screen/iOS13-Programmatic-Log-In-Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS13-Programmatic-Log-In-Screen
+//
+//  Created by Paul Solt on 3/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
